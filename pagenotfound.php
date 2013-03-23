a3ce87c2aef519580a7874216eb95e8c5dda598c
Isnaini D <ainaoe@yahoo.com>
2013-03-23 15:20:31 +0700

    Create pagenotfound.php

---
+++ pagenotfound.php
@@ -0,0 +1,19 @@
+<?php
+  session_start();
+	include "configuration/koneksi.php";
+?>
+<html>
+	<head>
+		<title>Page Not Found | SMP Negeri 10 Yogyakarta</title>
+		<link rel="stylesheet" href="style.css" type="text/css" />
+	</head>
+	<body>
+		<?php include "includes/header.php";?>
+		<?php include "includes/menu.php";?>
+		<div class="wrapper">
+		<h2>Page Not Found</h2>
+		<p>Maaf Halaman yang Anda cari tidak ditemukan, atau sudah dihapus.</p>
+		</div>
+		<?php include "includes/footer.php";?>
+	</body>
+</html>
